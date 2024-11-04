9c3c42ff0a0fc69d8eca5e3a5f06beafe3b9e3db
ohu9 <oliviah221@gmail.com>
2024-11-04 17:10:30 -0500

    title and button

--- screens/WelcomeScreen.js
+++ screens/WelcomeScreen.js
@@ -1,22 +1,27 @@
 import React from 'react'
-import { StyleSheet, ImageBackground, Button, Text, View } from 'react-native';
+import { StyleSheet, ImageBackground, TouchableOpacity, Text, View } from 'react-native';
 import { LinearGradient } from 'expo-linear-gradient';
 
 const WelcomeScreen = ({ navigation }) => {
   return (
-    <View style={styles.container}>
-		<LinearGradient
-				colors={['rgba(255, 255, 255, .1)', 'rgba(255, 249, 217, 1)']}
-				style={styles.gradient}
-			/>
+    <View style={styles.screen}>
 		<ImageBackground
 			style={styles.map}
 			source={require('../assets/map.png')}
 		>
-				<Button
-					title="Log in"
+			<View style={styles.content}>
+				<Text style={styles.title}>BuzzQuest</Text>
+				<TouchableOpacity
 					onPress={() => navigation.navigate('LogInScreen')}
-					style={styles.button} />
+					style={styles.button}
+				>
+					<Text style={{color: 'white', fontSize: 20, textAlign: 'center',}}>Play</Text>
+				</TouchableOpacity>
+			</View>
+			<LinearGradient
+					colors={['rgba(255, 255, 255, .1)', 'rgba(255, 249, 217, 1)']}
+					style={styles.gradient}
+				/>
 		</ImageBackground>
     </View>
   )
@@ -25,15 +30,13 @@ const WelcomeScreen = ({ navigation }) => {
 export default WelcomeScreen
 
 const styles = StyleSheet.create({
-	container: {
+	screen: {
 		flex: 1, 
 		alignItems: 'center', 
 		justifyContent: 'center',
-		backgroundColor: 'red',
 		position: 'absolute',
 		width: '100%',
 		height: '100%',
-		zIndex: 0,
 	},
 	map: {
 		width: '100%',
@@ -41,16 +44,31 @@ const styles = StyleSheet.create({
 		opacity: 1,
 		zIndex: 1,
 	},
-	button: {
-		position: 'absolute',
-		zIndex: 3,
-		width: '40px',
-		height: 'auto',
-	},
 	gradient: {
 		width: '100%',
 		height: '100%',
 		position: 'absolute',
 		zIndex: 2,
+	},
+	content: {
+		position: 'absolute',
+		top: '60%',
+		alignSelf: 'center',
+		zIndex: 3,
+	},
+	title: {
+		fontSize: 40, 
+		fontWeight: '600',
+		color: 'rgba(24, 46, 81, 1)',
+		marginBottom: '25%',
+	},
+	button: {
+		alignSelf: 'center',
+		width: 110,
+		height: 'auto',
+		paddingVertical: 9,
+		borderRadius: 17,
+		backgroundColor: 'rgba(24, 46, 81, 1)',
+		color: 'white',
 	}
   });
