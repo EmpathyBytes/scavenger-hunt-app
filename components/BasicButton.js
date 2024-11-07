f70b7269c0a9c2598b3abca596045062286693ba
Genesis Fu <genesis_fu@yahoo.com>
2024-11-06 23:42:13 -0500

    Adds system fonts, font sizes, colors, BasicButton component

---
+++ components/BasicButton.js
@@ -0,0 +1,45 @@
+import { COLORS, SIZES } from './theme';
+import { TouchableOpacity, StyleSheet, View, Text } from 'react-native';
+import { Figtree_400Regular, Figtree_600SemiBold, useFonts } from '@expo-google-fonts/figtree'
+
+export default function BasicButton({ text, backgroundColor, textColor, onPress }) {
+    //load font
+    const [fontsLoaded] = useFonts({
+        Figtree_400Regular,
+        Figtree_600SemiBold,
+    });
+
+    if (!fontsLoaded) {
+        return null;
+    }
+
+    return (
+        <TouchableOpacity style={[{backgroundColor: backgroundColor}, styles.container]} onPress={onPress}>
+            <Text style={[{color: textColor}, styles.text]}>
+                {text}
+            </Text>
+        </TouchableOpacity>
+    );
+}
+
+//example button component
+{/* <BasicButton
+        text="Welcome"
+        backgroundColor={COLORS.navy}
+        textColor={COLORS.beige}
+        onPress={() => navigation.navigate('LogInScreen')}/> */}
+
+const styles = StyleSheet.create({
+    container: {
+        //flex: 1,
+        alignItems: "center",
+        justifyContent: "center",
+        paddingVertical: 13,
+        width: "80%",
+        borderRadius: 15,
+    },
+    text: {
+        fontSize: SIZES.body,
+        fontFamily: "Figtree_400Regular",
+    }
+})
---
+++ components/theme.js
@@ -0,0 +1,18 @@
+const COLORS = {
+    beige: "#FFF9D9",//beige
+    navy: "#182E51",//navy
+    gold: "#EEB210",//gold/yellow
+  
+    gray: "#D6DBD4",
+  
+    white: "#FFFFFF",
+    red: "#E04F39",
+};
+
+const SIZES = {
+    title: 50,
+    heading: 35,
+    body: 20,
+    body_small: 15,
+};
+export { COLORS, SIZES }
