798793e76d3110a9872954a6c8216d5de5f94153
yooreo <victoria4318@gmail.com>
2025-04-10 19:48:38 -0400

    Fully implemented signup screen

--- screens/SignUpScreen.js
+++ screens/SignUpScreen.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import { 
   StyleSheet, ImageBackground, Image, Text, View, TextInput, 
-  ActivityIndicator, Alert 
+  ActivityIndicator, Alert, Dimensions
 } from 'react-native';
 import { LinearGradient } from 'expo-linear-gradient';
 import { createUserWithEmailAndPassword } from 'firebase/auth';
@@ -9,7 +9,6 @@ import { auth } from '../firebase_config';
 import { COLORS, SIZES } from '../components/theme';
 import { Figtree_400Regular, Figtree_600SemiBold, useFonts } from '@expo-google-fonts/figtree';
 import BasicButton from '../components/BasicButton';
-
 
 const SignUpScreen = ({ navigation }) => {
 	const [email, setEmail] = useState('');
@@ -23,6 +22,8 @@ const SignUpScreen = ({ navigation }) => {
 	  Figtree_600SemiBold,
 	});
   
+	const screenHeight = Dimensions.get('window').height;
+
 	// Email validation using regex
 	const validateEmail = (email) => {
 	  const re = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
@@ -88,7 +89,7 @@ const SignUpScreen = ({ navigation }) => {
 	  }
 	};
 	return (
-		<View style={styles.container}>
+		<View style={styles.screen}>
 		  <ImageBackground
 			style={styles.map}
 			source={require('../assets/map.png')}
@@ -98,14 +99,11 @@ const SignUpScreen = ({ navigation }) => {
 			  colors={['rgba(255, 255, 255, .1)', 'rgba(255, 249, 217, 1)']}
 			  style={styles.gradient}
 			/>
-			<Image
-			  style={styles.bee}
-			  source={require('../assets/bee.png')}
-			/>
-			<Image
-			  style={styles.mapLine}
-			  source={require('../assets/map-line.png')}
-			/>
+			<View style={styles.content}>
+			  <Image style={styles.bee} source={require('../assets/bee.png')}/>
+			  <Image style={styles.mapLine} source={require('../assets/map-line.png')}/>
+			  <Image style={styles.cross} source={require('../assets/cross.png')}/>
+			</View>
 			<View style={styles.box}>
 			  <View style={styles.signUpBox}>
 				<Text style={styles.title}>Welcome!</Text>
@@ -162,105 +160,111 @@ const SignUpScreen = ({ navigation }) => {
 
 export default SignUpScreen;
 
+const width = Dimensions.get('window').width;
+const height = Dimensions.get('window').height;
+
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    fontFamily: 'Figtree_600SemiBold',
-  },
-  map: {
-    flex: 1,
-  },
-  gradient: {
-    width: '100%',
-    height: '100%',
-    position: 'absolute',
-  },
-  bee: {
-    height: 100,
-    objectFit: 'contain',
-    position: 'absolute',
-    alignSelf: 'center',
-    top: '15%',
-    zIndex: 0,
-    opacity: 0.7,
-  },
-  mapLine: {
-    alignSelf: 'center',
-    height: 550,
-    objectFit: 'contain',
-    position: 'absolute',
-    top: '25%',
-    zIndex: 0,
-    opacity: 0.5,
-  },
-  box: {
-    backgroundColor: COLORS.beige,
-    borderRadius: 20,
-    alignSelf: 'center',
-    zIndex: 2,
-    height: 520,
-    marginTop: 180,
-  },
-  signUpBox: {
-    position: 'center',
-    top: '22.5%',
-    alignItems: 'center',
-  },
-  title: {
-    fontFamily: 'Figtree_400Regular',
-    fontSize: SIZES.title,
-    color: COLORS.navy,
-    width: '70%',
-    paddingBottom: 100,
-    marginTop: -100,
-    marginBottom: 5,
-    textAlign: 'center',
-    alignSelf: 'center',
-  },
-  subtitle: {
-    fontSize: SIZES.body,
-    fontFamily: 'Figtree_400Regular',
-    color: COLORS.darkGray,
-    marginTop: -70,
-    marginBottom: 30,
-    textAlign: 'center',
-    alignSelf: 'center',
-  },
-  input: {
-    width: '80%',
-    height: 50,
-    borderColor: COLORS.darkGray,
-    borderWidth: 2,
-    borderRadius: 15,
-    paddingHorizontal: 15,
-    marginBottom: 20,
-    fontFamily: 'Figtree_400Regular',
-    fontSize: SIZES.body,
-    alignSelf: 'center',
-  },
-  errorText: {
-    color: 'red',
-    fontSize: SIZES.body_small,
-    textAlign: 'center',
-    marginBottom: 10,
-  },
-  logInText: {
-    marginBottom: 200,
-    fontFamily: 'Figtree_400Regular',
-    fontSize: SIZES.body_small,
-    textAlign: 'center',
-    marginTop: 10,
-  },
-  signupLink: {
-    color: COLORS.primary,
-    fontFamily: 'Figtree_600SemiBold',
-  },
-  signUpButton: {
-    alignSelf: 'center',
-    justifyContent: 'center',
-    alignItems: 'center',
-    textAlign: 'center',
-    flex: 1,
-    width: '50%',
-  },
+	screen: {
+		flex: 1,
+		fontFamily: 'Figtree_600SemiBold',
+	},
+	map: {
+		flex: 1,
+		alignContent: 'center',
+		justifyContent: 'center',
+	},
+	content: {
+		height: '100%',
+		flexDirection: 'column',
+		justifyContent: 'center',
+		alignContent: 'center',
+	},
+	bee: {
+		height: '15%',
+		objectFit: 'contain',
+		alignSelf: 'center',
+		zIndex: 1,
+		marginBottom: -50,
+		marginLeft: 30,
+	},
+	mapLine: {
+		alignSelf: 'center',
+		height: '76%',
+		objectFit: 'contain',
+	},
+	cross: {
+		height: '12%',
+		width: '20%',
+		objectFit: 'contain',
+		alignSelf: 'center',
+		zindex: 1,
+	},
+	gradient: {
+		width: '100%',
+		height: '100%',
+		position: 'absolute',
+	},
+	box: {
+		backgroundColor: COLORS.beige,
+		borderRadius: 20,
+		alignSelf: 'center',
+		position: 'absolute',
+		justifyContent: 'center',
+		alignItems: 'center',
+		zIndex: 2,
+		width: width*.9,
+		marginTop: (height-550)/2,
+	},
+	signUpBox: {
+		width: '90%',
+		alignItems: 'center',
+		padding: width * 0.05,
+	},
+	title: {
+		fontFamily: 'Figtree_400Regular',
+		fontSize: width * 0.09,
+		color: COLORS.navy,
+		textAlign: 'center',
+		marginBottom: height * 0.02,
+		zIndex: 2,
+	},
+	subtitle: {
+		fontSize: width * 0.05,
+		fontFamily: 'Figtree_400Regular',
+		color: COLORS.darkGray,
+		textAlign: 'center',
+		marginBottom: height * 0.03,
+	},
+	input: {
+		width: '85%',
+		height: height * 0.06,
+		borderColor: COLORS.darkGray,
+		borderWidth: 2,
+		borderRadius: 15,
+		paddingHorizontal: width * 0.04,
+		fontSize: width * 0.04,
+		marginBottom: height * 0.02,
+	},
+	errorText: {
+		color: 'red',
+		fontSize: SIZES.body_small,
+		textAlign: 'center',
+		marginBottom: 10,
+	},
+	logInText: {
+		fontSize: width * 0.04,
+		textAlign: 'center',
+		marginTop: height * 0.015,
+		marginBottom: height * 0.05,
+	},
+	signupLink: {
+		color: COLORS.primary,
+		fontFamily: 'Figtree_600SemiBold',
+	},
+	signUpButton: {
+		width: '60%',
+		height: height * 0.06,
+		justifyContent: 'center',
+		alignItems: 'center',
+	},
 });
