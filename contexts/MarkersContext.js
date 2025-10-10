701549984f3c02db5878a854ea91f8c8f6a6ca67
eileenyang10 <eileenyang10@gmail.com>
2025-10-10 18:23:50 -0400

    Added Geofencing to check if you entered specified regions

--- contexts/MarkersContext.js
+++ contexts/MarkersContext.js
@@ -3,7 +3,20 @@ import React, { createContext, useRef } from 'react';
 export const MarkersContext = createContext(null);
 
 export const MarkersProvider = ({ children }) => {
-    const markers = useRef([]); // State to hold markers
+    const markers = useRef([
+        {
+            key: 'm1',
+            coordinate: { latitude: 33.77011035444347, longitude: -84.3927472105032 },
+            title: 'Clue 1',
+            description: 'Find the statue'
+        },
+        {
+            key: 'm2',
+            coordinate: { latitude: 33.77046811985075, longitude: -84.39443900124264 },
+            title: 'Clue 2',
+            description: 'Find the building'
+        }
+]); // State to hold markers
     
     return (
         <MarkersContext.Provider value={{ markers }}>
