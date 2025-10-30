fbc0c39c458d07f2c8302370a86870a3ca7364eb
Maryam Wadood <maryamwadoodgt@gmail.com>
2025-10-30 00:01:03 -0400

    #81 Artifact Info Screen

--- screens/ArtifactInfoScreen.js
+++ screens/ArtifactInfoScreen.js
@@ -1,10 +1,10 @@
 import React from 'react'
-import { Text, View, StyleSheet } from 'react-native';
-import { COLORS, SIZES } from '../../components/theme';
+import { Text, View, StyleSheet, Image } from 'react-native';
+import { COLORS, SIZES } from '../components/theme';
 import { Figtree_400Regular, Figtree_600SemiBold, useFonts } from '@expo-google-fonts/figtree' //font
-import BackButton from '../../components/BackButton';
+import BackButton from '../components/BackButton';
 
-const ArtifactInfoScreen = ({ navigation }) => {
+const ArtifactInfoScreen = ({ navigation, setScreenIndex }) => {
 
     //load font
     const [fontsLoaded] = useFonts({
@@ -18,10 +18,10 @@ const ArtifactInfoScreen = ({ navigation }) => {
 
     return (
         <View style={styles.container}>
-            <BackButton style={{marginBottom: 20, size:48}} backgroundColor={COLORS.beige} onPress={() => {setScreenIndex(1)}} />
+            <BackButton style={{marginBottom: 20, size:48}} backgroundColor={COLORS.beige} onPress={() => {setScreenIndex(2)}} />
 
-            <Text style={styles.infoLabel}> Buzz's Converse: </Text>
-            <Image source={require('../../assets/buzzconverse.png')} style={styles.image} />
+            <Text style={styles.infoLabel}> Buzz's Converse </Text>
+            <Image source={require('../assets/buzzconverse.png')} style={styles.image} />
             <Text style={styles.infoLabel}> Fun Fact: </Text>
             <Text style={styles.infoText}>
                 When Adidas took over as Georgia Tech's apparel provider in 2018, Buzz ditched his old Converse Chuck's for some custom-designed Adidas sneakers!
@@ -40,12 +40,15 @@ const styles = StyleSheet.create({
         fontFamily: "Figtree_600SemiBold",
         fontSize: 30,
         marginBottom: 10,
+        textAlign: 'center',
     },
     infoText: {
         fontFamily: "Figtree_400Regular",
         fontSize: 20,
         marginBottom: 20,
         textAlignVertical: "center",
+        paddingHorizontal: 20,
+        textAlign: 'center',
     }
 });
 
